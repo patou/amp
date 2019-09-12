625030ed1ec3821c233e7316dde278cacf1e1f61
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-09-12 17:12:47 +0200

    typescript

--- functions/src/sendemail.ts
+++ functions/src/sendemail.ts
@@ -2,7 +2,6 @@ import * as functions from 'firebase-functions';
 
 function onSendEmail(req: functions.Request, res: functions.Response) {
   console.log(`Email sent to ${req.query.email}`);
-  res.status = 200
-  res.body = `Email sent to ${req.query.email}`
+  res.status(200).send(`Email sent to ${req.query.email}`);
 }
 export const sendEmail = functions.https.onRequest(onSendEmail);
