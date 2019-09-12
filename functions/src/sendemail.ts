1630e5ecfa2accde76ab96623f5a447aa7439a1c
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-09-12 18:07:48 +0200

    mailgun

--- functions/src/sendemail.ts
+++ functions/src/sendemail.ts
@@ -3,16 +3,14 @@ import * as mailgun from 'mailgun-js';
 
 const mg = mailgun({apiKey: functions.config().mailgun.key, domain: 'todo.patou.dev'});
 
-function onSendEmail(req: functions.Request, res: functions.Response) {
+async function onSendEmail(req: functions.Request, res: functions.Response) {
   const data = {
     from: 'Todo Amp <amp@todo.patou.dev>',
     to: req.query.email,
     subject: 'Hello',
     text: 'Testing some Mailgun awesomness!'
   };
-  mg.messages().send(data, function (error : any, body: any) {
-    console.log(body);
-  });
+  await mg.messages().send(data);
   console.log(`Email sent to ${req.query.email}`);
   res.status(200).send(`Email sent to ${req.query.email}`);
 }
